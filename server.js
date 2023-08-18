8b2d0e598a65ede2464ef2f0bd0916a6a0ba6ee3
Justin M <manningjustin08@yahoo.com>
2023-08-18 01:34:19 -0400

    Second commit, finished all my 10 routes

--- server.js
+++ server.js
@@ -17,6 +17,39 @@ app.get('/tia', (req, res) => {
     res.send(' <h1> Tias Page </h1>')
 })
 
+app.get('/omar', (req, res) => {
+    res.send(' <h1> Omars Page </h1>')
+})
+
+app.get('/sharla', (req, res) => {
+    res.send(' <h1> Sharlas Page </h1>')
+})
+
+app.get('/LJ', (req, res) => {
+    res.send(' <h1> LJs Page </h1>')
+})
+
+app.get('/kai', (req, res) => {
+    res.send(' <h1> Kais Page </h1>')
+})
+
+app.get('/taylor', (req, res) => {
+    res.send(' <h1> Taylors Page </h1>')
+})
+
+app.get('/mariecia', (req, res) => {
+    res.send(' <h1> Mariecias Page </h1>')
+})
+
+app.get('/keiara', (req, res) => {
+    res.send(' <h1> Keiaras Page </h1>')
+})
+
+app.get('/liani', (req, res) => {
+    res.send(' <h1> Lianis Page </h1>')
+})
+
+
 
 
 
